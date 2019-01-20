87aff7756c38354d7f3a0c38a8bcd6f6ec86a8af
maguette <maguettetop96@gmail.com>
2019-01-20 11:32:06 +0100

    Update Yield_Computation.h

--- Yield_Computation.h
+++ Yield_Computation.h
@@ -7,46 +7,44 @@
 
 using namespace std;
 
-double f(double PMT, double PV, int n, double r) {
-    double temp;
-    temp = PMT / (r*(1+(1/(pow(1+r,n)-1)))) - PV ;
-    return temp;
-}
-
-double approximateYTM(double PMT, double PV, int n){
-    return (PMT -(PV)/(n))/(0.5*(PV));
-}
-
-double YTM(){
-    double PMT,PV,r,k,rNext, temp;
-    int n;
-
-    //PROVIDING VALUES
-    //double PV, PMT;
-    //int n;
-
-    cout << "Please provide:" << endl;
-    cout << "The Present Value:"; cin >> PV;
-    cout << "PMT:"; cin >> PMT;
-    cout << "The number of periods"; cin >> n ;
-    //PV = PresentValue;
-
-    cout << "PMT: " << PMT << endl;
-    cout << "n: " << n << endl;
-
-    // starting points
-    r = approximateYTM(PMT,PV,n);
-    rNext = 0.95*r;
-
-    while (abs(f(PMT,PV,n,rNext))> 0.001){
-        temp = rNext;
-        rNext = rNext - (f(PMT,PV,n,rNext)*(rNext - r))/(f(PMT,PV,n,rNext)-f(PMT,PV,n,r));
-        r = temp;
-
+    double f(double PMT, double PV, int n, double r) {
+        double temp;
+        temp = PMT / (r*(1+(1/(pow(1+r,n)-1)))) - PV ;
+        return temp;
     }
 
-    cout << "objective function: " << f(PMT,PV,n,rNext) << endl;
-    cout << "YTM : " << (rNext * k)*100 << "% ." << endl;
-}
+    double approximateYTM(double PMT, double PV, int n){
+        return (PMT -(PV)/(n))/(0.5*(PV));
+    }
 
+    double YTM(){
+        double PMT,PV,r,rNext,k, temp;
+        int n;
+
+        //PROVIDING VALUES
+
+        cout << "Please provide:" << endl;
+        cout << "The Present Value:"; cin >> PV;
+        cout << "The payment PMT:"; cin >> PMT;
+        cout << "The number of periods"; cin >> n ;
+        cout << "Periods Per Year:"; cin >> k;
+
+        cout << "PMT: " << PMT << endl;
+        cout << "n: " << n << endl;
+     
+
+        // starting points
+        r = approximateYTM(PMT,PV,n);
+        rNext = 0.95*r;
+
+        while (abs(f(PMT,PV,n,rNext))> 0.001){
+            temp = rNext;
+            rNext = rNext - (f(PMT,PV,n,rNext)*(rNext - r))/(f(PMT,PV,n,rNext)-f(PMT,PV,n,r));
+            r = temp;
+
+        }
+
+        cout << "objective function: " << f(PMT,PV,n,rNext) << endl;
+        cout << "YTM : " << (rNext * k)*100 << "% ." << endl;
+    }
 #endif //UNTITLED9_YIELD_COMPUTATION_H
